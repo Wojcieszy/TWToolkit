d3767ad0ca2e6ca4a30662ba4346b8aec94faf82
Thathanka Iyothanka <thathanka.tw@gmail.com>
2015-11-28 15:04:02 +0100

    Update noti.js

--- noti.js
+++ noti.js
@@ -1,6 +1,6 @@
 TWToolkit.Reminder.history_noti=[
     ['<strong>Dziki The West</strong></br>Check out our new article about Halloween 2015 !<img src="https://i.imgur.com/Tk3WCAp.png"/>','https://i.imgur.com/Tk3WCAp.png','https://dzikitw.wordpress.com/eventy/halloween-2015/'],
-      ['<strong>Dziki The West</strong></br>Check out our new article about Fort Battles !</br><img style="height:100px;" src="https://westfrs.innogamescdn.com/images/map/texture_fort_05.png"/>','https://i.imgur.com/Tk3WCAp.png','https://dzikitw.wordpress.com/eventy/halloween-2015/'],
+      ['<strong>Dziki The West</strong></br>Check out our new article about Fort Battles !</br></br><img style="height:100px;text-align:center;" src="https://westfrs.innogamescdn.com/images/map/texture_fort_05.png"/>','https://i.imgur.com/Tk3WCAp.png','https://dzikitw.wordpress.com/eventy/halloween-2015/'],
 
   //['<strong>title</strong></br>text','img','link'],
   ]
