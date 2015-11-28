0e28ed930863d849b7974b7553bd30b001b0273b
Thathanka Iyothanka <thathanka.tw@gmail.com>
2015-11-28 14:56:55 +0100

    Update noti.js

--- noti.js
+++ noti.js
@@ -1,1 +1,1 @@
-TWToolkit.Reminder.history_noti=[['<strong>Dziki T-W</strong></br></br><i> Check out our new article !</i>','https://i.imgur.com/Tk3WCAp.png','https://dzikitw.wordpress.com/eventy/halloween-2015/']]
+TWToolkit.Reminder.history_noti=[['<strong>Dziki The West</strong></br><i> Check out our new article about Halloween 2015 !</i>','https://i.imgur.com/Tk3WCAp.png','https://dzikitw.wordpress.com/eventy/halloween-2015/']]
