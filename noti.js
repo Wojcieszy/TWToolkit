65cea7d3fb527598b2c6cbc6085ba46ccff2a0b2
Wojcieszy <wojtek_debski@wp.pl>
2015-11-28 15:33:55 +0100

    Update noti.js

--- noti.js
+++ noti.js
@@ -1,5 +1,5 @@
 TWToolkit.Reminder.history_noti=[
-    ['<strong>Dziki The West</strong></br>Check out our new article about Fort Battles !</br></br><img style="height:100px;display: block;margin-left: auto;margin-right: auto;" src="https://westfrs.innogamescdn.com/images/map/texture_fort_05.png"/>','https://i.imgur.com/Tk3WCAp.png','https://dzikitw.wordpress.com/eventy/halloween-2015/'],
+    ['<strong>Dziki The West</strong></br>Czy widziales proadniki dotyczace fortow?!</br></br><img style="height:100px;display: block;margin-left: auto;margin-right: auto;" src="https://westfrs.innogamescdn.com/images/map/texture_fort_05.png"/>','https://i.imgur.com/Tk3WCAp.png','https://dzikitw.wordpress.com/forty/'],
     
     //['<strong>title</strong></br>text','img','link'],
     // Don't forget , or [] very important ;)
